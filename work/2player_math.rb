cecc0b047ec9cf30ab2937c91e08d45714cafbaf
car <c_chow568@yahoo.ca>
2016-02-04 20:20:53 -0800

    Added draw scenario to 2 player game

--- work/2player_math.rb
+++ work/2player_math.rb
@@ -4,16 +4,22 @@
 # game ends when one user looses all their lives and game calcs who won
 
 class MathGame
-  attr_reader :player1, :player2, :current_player
+  attr_reader :player1, :player2, :current_player, :game_ongoing
   attr_accessor :answer, :num1, :num2
 
   def initialize
     @player1 = {score: 0, lives: 3}
     @player2 = {score: 0, lives: 3}
+    @current_player = @player1
+    @game_ongoing = true
   end
 
   def get_player
     @current_player == @player1 ? "Player 1" : "Player 2"
+  end
+
+  def switch_player 
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
 
   def generate_question
@@ -24,12 +30,8 @@ class MathGame
     "#{get_player}: What does #{@num1} plus #{@num2} equal?"
   end
 
-  def switch_player 
-    @current_player = @current_player == @player1 ? @player2 : @player1
-  end
-
   def prompt_player
-    generate_question
+    puts generate_question
     @answer = gets.chomp
   end
 
@@ -37,13 +39,31 @@ class MathGame
     @current_player[:lives] == 0
   end
 
+  def print_lives
+    puts "Player1 lives left: #{@player1[:lives]}"
+    puts "Player2 lives left: #{@player2[:lives]}"
+  end
+
+  def print_stats
+    puts "Player 1"
+    puts "\tScore: #{@player1[:score]}"
+    puts "\tLives left: #{@player1[:lives]}"
+
+    puts "Player 2"
+    puts "\tScore: #{@player2[:score]}"
+    puts "\tLives left: #{@player2[:lives]}"
+  end
+
   def declare_winner
+    winner = @player1[:score] > @player2[:score] ? "Player 1" : "Player 2"
+    draw = @player1[:score] == @player2[:score]
+    @game_ongoing = false
+
+    draw ? "It's a draw!" : "#{winner} wins!" 
   end
 
   def loose_life(player)
-    # change current player's life if needed
     player[:lives] -= 1
-    # check if anyone lost all their lives yet (game_end?)
     declare_winner if game_end? 
   end
 
@@ -51,23 +71,23 @@ class MathGame
     @answer.to_i == @num1 + @num2
   end
 
-  def change_score
-    # if answer is accurate, add score to the right player
-    right_answer? ? @current_player[:score] += 1 : loose_life(@current_player)
-    # if answer is wrong, subtract a life (loose_life)
-  end
-
   def verify_answer
-    # make sure answer is a numeric
-    # check answer is accurate (get_score)
-    loose_life if !right_answer
-    change_score
-    switch_player 
-    # once answer verified, switch players
+    right_answer? ? @current_player[:score] += 1 : loose_life(@current_player)
   end
 
   def play
-    # REPL all dis
-  end
+    begin 
+      prompt_player
+      verify_answer
+      print_stats
+      switch_player 
+    end while @game_ongoing
 
+    puts declare_winner
+    puts "Final score: "
+    print_stats
+  end
 end
+
+game = MathGame.new
+game.play
