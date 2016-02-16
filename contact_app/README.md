4d4670de70939656edd18637156563e3df980807
car <c_chow568@yahoo.ca>
2016-02-16 13:12:34 -0800

    Added basic object-relational mapping for create, update, find, and search.'

---
+++ contact_app/README.md
@@ -0,0 +1,12 @@
+Contact List 
+============
+
+To run in terminal: ./contact_list [command]
+
+where [command] is one of the commands below:
+
+new [name] [email] - Create new contact with a name & email
+update [id] - Update an existing contact with a specific ID
+list - List all contacts
+show [id] - Show a specific contact by ID
+search [term] - Find contacts based on a value [e.g. name]
--- contact_app/contact.rb
+++ contact_app/contact.rb
@@ -1,18 +1,25 @@
-require 'csv'
+require 'pg'
 
 # Represents a person in an address book.
 class Contact
-  # diff class dealing w just csv and contact class will call it [if want to use diff kind of file]
-  @@filename = 'contacts.csv'
   attr_reader :id
   attr_accessor :name, :email, :phone
-  def initialize(id, name, email)
+  def initialize(name, email, id=nil)
     @id = id
     @name = name
     @email = email   
     @phone = []
   end
   
+  def save
+    if id.nil?  
+      query = Contact.connection.exec_params("INSERT INTO contacts (name,email) VALUES($1, $2) RETURNING id;", [name,email])
+      @id = query[0]['id']
+    else
+      Contact.connection.exec_params("UPDATE contacts SET name=$1, email=$2 WHERE id=$3;", [@name, @email, @id])
+    end
+  end  
+
   def set_phone(entry)
     # etnry format looks like: "label: 123-4567"
     entry = entry.split(' ')
@@ -21,6 +28,15 @@ class Contact
   
   # Provides functionality for managing a list of Contacts in a database.
   class << self
+    def connection
+      conn = PG.connect(
+        host: 'localhost',
+        dbname: 'contacts',
+        user: 'testuser',
+        password: 'supworld'
+      );
+    end
+
    def duplicate_email?(email)
      CSV.foreach(@@filename) do |contact|
        return true if contact.include?(email)
@@ -35,65 +51,47 @@ class Contact
 
    # Returns an Array of Contacts loaded from the database.
    def all 
-      CSV.read(@@filename)
+      contacts = []
+      results = connection.exec_params('SELECT * FROM contacts;') 
+      results.each do |contact|
+        contacts << Contact.new(contact['name'], contact['email'], contact['id'])
+      end 
+      contacts
     end
   
     # Creates a new contact, adding it to the database, returning the new contact.
-    def create(name, email, *phone)
-      check_duplicate_email!(email)
-
-      file =  CSV.read(@@filename)
-      # id based on number of lines in the file  
-      id = file.length + 1
-      person = Contact.new(id, name, email)         
-
-      new_contact = [person.id, person.name, person.email] 
-
+    def create(name, email)
+      # TODO: implement!
+      #check_duplicate_email!(email)
+      new_contact = self.new(name, email)         
+      new_contact.save
+      # TODO: implement this!
       # add each phone number to new_contact if exists
-      unless phone.empty? 
-        person.phone = phone
-        phone.flatten.each do |number|
-          new_contact <<  number
-        end
-      end 
-   
-      CSV.open(@@filename, 'a') do |contact|
-        contact << new_contact 
-      end
-      person
+      #unless phone.empty? 
+      #  person.phone = phone
+      #  phone.flatten.each do |number|
+      #    new_contact <<  number
+      #  end
+      #end 
     end
 
     # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO; return the obj instead of a string
-      result = nil
-     
-      CSV.foreach(@@filename) do |contact|
-        if contact[0].to_i == id
-          result = contact
-        end
-      end
-      result
+      #result = nil
+      result = connection.exec_params("SELECT * FROM contacts WHERE id=$1 LIMIT 1;", [id])
+      contact = result[0]
+      self.new(contact['name'], contact['email'], contact['id'])
     end
 
     # Returns an array of contacts who match the given term.
     def search(term)
-      CSV.foreach(@@filename) do |contact|
-        contact.each do |row|
-           return contact if row.include?(term)
-        end
+      results = connection.exec_params("SELECT * FROM contacts WHERE name LIKE concat('%', $1::text, '%') OR email LIKE concat('%', $1::text, '%');", [term])
+      contacts = []
+      results.each do |contact|
+        contacts << Contact.new(contact['name'], contact['email'],contact['id'])
       end
-      #all_contacts = CSV.read(@@filename)
-      #matched_rows = all_contacts.select do |row|
-       #               row.any? do |column|
-       #                column.include?(term)
-       #              end
-       #           end
-      # converts every value in the old array into a Contact object
-      #matched_rows.map do |row|
-        # TODO: account for phone #s
-       # Contact.new(row[0], row[1], row[2])
-      #end
+      contacts
+      # TODO: account for phone #s
     end
   end
 
