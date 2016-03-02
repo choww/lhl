8f1fca5c5f5bf16ccfcc5a4adb5ed818a5882495
car <c_chow568@yahoo.ca>
2016-03-01 20:47:25 -0800

    Rotten Mangoes - pagination

--- rotten-mangoes/app/controllers/admin/users_controller.rb
+++ rotten-mangoes/app/controllers/admin/users_controller.rb
@@ -2,7 +2,13 @@ class Admin::UsersController < ApplicationController
   before_action :require_admin
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(2)
+  end
+
+  def create_admin
+    @user = User.find(params[:id])
+    @user.toggle!(:admin?)
+    redirect_to admin_users_path
   end
 
   def destroy
@@ -16,5 +22,11 @@ class Admin::UsersController < ApplicationController
         flash[:danger] = "You don't have access to this page"
         redirect_to root_path
       end
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :firstname, :lastname, :email, :admin?, :password
+      )
     end
 end
--- rotten-mangoes/db/seeds.rb
+++ rotten-mangoes/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(firstname: 'dude', lastname: 'bro', email: 'dude@bro.ca', password: 'canada')
+User.create(firstname: 'files', lastname: 'folders', email: 'files@folders.gov', password: 'canada')
+User.create(firstname: 'nil', lastname: 'class', email: 'nil@class.ca', password: 'canada')
